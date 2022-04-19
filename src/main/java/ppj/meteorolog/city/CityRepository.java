f384c28069cbeb09257f2ee2f31de1320a21a187
Ondřej Vacek <ondrej.vacek@entry-cz.com>
2022-04-19 21:04:37 +0200

    Add repository interface for City and Country

---
+++ src/main/java/ppj/meteorolog/city/CityRepository.java
@@ -0,0 +1,7 @@
+package ppj.meteorolog.city;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, CityId> { }
---
+++ src/main/java/ppj/meteorolog/country/CountryRepository.java
@@ -0,0 +1,7 @@
+package ppj.meteorolog.country;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, String> { }
