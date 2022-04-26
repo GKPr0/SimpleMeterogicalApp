2bf661c11b353971938c1971e9f029a8202cf4e3
Ondřej Vacek <ondrej.vacek@entry-cz.com>
2022-04-26 18:28:51 +0200

    Move rest exception handling to global exception handler

--- src/main/java/ppj/meteorolog/shared/BaseApiController.java
+++ src/main/java/ppj/meteorolog/shared/BaseApiController.java
@@ -1,14 +1,6 @@
 package ppj.meteorolog.shared;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class BaseApiController {
 
@@ -24,17 +16,5 @@ public class BaseApiController {
             return ResponseEntity.notFound().build();
 
         return ResponseEntity.badRequest().body(result.getError());
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
     }
 }
