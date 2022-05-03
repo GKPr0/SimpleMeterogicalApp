771b7ead8601a5c7ca8eb932a14e67c2ec5dc055
Ondřej Vacek <ondrej.vacek@entry-cz.com>
2022-05-03 20:48:19 +0200

    Add Weather repository

--- src/main/java/ppj/meteorolog/weather/WeatherMonitor.java
+++ src/main/java/ppj/meteorolog/weather/WeatherMonitor.java
@@ -5,9 +5,10 @@ import com.influxdb.client.WriteApiBlocking;
 import com.influxdb.client.domain.WritePrecision;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import ppj.meteorolog.city.City;
+import ppj.meteorolog.city.CityRepository;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -18,29 +19,28 @@ public class WeatherMonitor {
     private final InfluxDBClient influxDBClient;
     private final Logger log = LoggerFactory.getLogger(this.getClass());
     private final UUID id = UUID.randomUUID();
-    private final WeatherRepository weatherRepository;
+    private final CityRepository cityRepository;
 
-    @Autowired
-    public WeatherMonitor(InfluxDBClient influxDBClient, WeatherRepository weatherRepository) {
+    public WeatherMonitor(InfluxDBClient influxDBClient, CityRepository cityRepository) {
         this.influxDBClient = influxDBClient;
-        this.weatherRepository = weatherRepository;
+        this.cityRepository = cityRepository;
     }
 
     @Scheduled(fixedRate = 5000)
     public void writeTest() {
         int count = (int) (Math.random() * 100);
 
+        City Liberec = cityRepository.findCityByNameAndCountry_Code("Liberec", "CZ").get();
+
         WeatherMeasurement measurement = new WeatherMeasurement();
         measurement.setTemperature(Math.random() * 40);
         measurement.setPressure(Math.random() * 300 + 800);
         measurement.setHumidity(Math.random() * 100);
-        measurement.setCityID(id);
+        measurement.setCityID(Liberec.getId());
         measurement.setTimestamp(Instant.now());
 
         WriteApiBlocking writeApiBlocking = influxDBClient.getWriteApiBlocking();
         writeApiBlocking.writeMeasurement(WritePrecision.NS, measurement);
         log.info("New measurement logged with value：" + count);
-
-        WeatherMeasurement weatherMeasurement = weatherRepository.findLastMeasurementForCity(id);
     }
 }
--- src/main/java/ppj/meteorolog/weather/WeatherRepository.java
+++ src/main/java/ppj/meteorolog/weather/WeatherRepository.java
@@ -1,12 +1,18 @@
 package ppj.meteorolog.weather;
 
+import com.influxdb.client.DeleteApi;
 import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.WriteApiBlocking;
+import com.influxdb.client.domain.WritePrecision;
 import com.influxdb.query.FluxRecord;
 import com.influxdb.query.FluxTable;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -14,38 +20,73 @@ public class WeatherRepository {
 
     private final InfluxDBClient influxDBClient;
 
-    @Autowired
     public WeatherRepository(InfluxDBClient influxDBClient) {
         this.influxDBClient = influxDBClient;
     }
 
-    public WeatherMeasurement findLastMeasurementForCity(UUID cityId) {
-        String query = "from(bucket: \"Development\") " +
-                "|> range(start: -1d) " +
-                "|> filter(fn: (r) => r._measurement == \"weather\" and r.cityID == \"" + cityId + "\" )" +
-                "|> filter(fn: (r) => r._field == \"humidity\" or r._field == \"temperature\" or r._field == \"pressure\")" +
-                "|> last() " +
-                "|> pivot(rowKey: [\"_stop\"], columnKey: [\"_field\"], valueColumn: \"_value\")";
+    public void save(WeatherMeasurement measurement) {
+        WriteApiBlocking writeApiBlocking = influxDBClient.getWriteApiBlocking();
+        writeApiBlocking.writeMeasurement(WritePrecision.NS, measurement);
+    }
+
+    public void delete(WeatherMeasurement measurement) {
+        String bucket = "Development";
+        String org = "Development";
+        String predicate = "_measurement = \"weather\" and cityID = \"" + measurement.getCityID() + "\"";
+        OffsetDateTime startTime = measurement.getTimestamp().atOffset(ZoneOffset.UTC);
+        OffsetDateTime endTime = startTime.plusNanos(1);
+
+        DeleteApi deleteApi = influxDBClient.getDeleteApi();
+        deleteApi.delete(startTime, endTime, predicate, bucket, org);
+    }
+
+    public Optional<WeatherMeasurement> findMeasurementForCityByTimestamp(UUID cityId, Instant timestamp) {
+        String query = "from(bucket: \"Development\")" +
+                "  |> range(start: " + timestamp + ", stop: " + timestamp.plusNanos(1) + ")" +
+                "  |> filter(fn: (r) => r[\"_measurement\"] == \"weather\")" +
+                "  |> filter(fn: (r) => r[\"_field\"] == \"humidity\" or r[\"_field\"] == \"pressure\" or r[\"_field\"] == \"temperature\")" +
+                "  |> filter(fn: (r) => r[\"cityID\"] == \"" + cityId + "\")" +
+                "  |> pivot(rowKey: [\"_time\"], columnKey: [\"_field\"], valueColumn: \"_value\")";
 
         List<FluxTable> tables = influxDBClient.getQueryApi().query(query);
+        if(tables.size() == 0)
+            return Optional.empty();
+
         FluxRecord result = tables.get(0).getRecords().get(0);
 
         return mapFluxRecordToWeatherMeasurement(result, cityId);
     }
 
-    public WeatherMeasurement findLastDayAverageForCity(UUID cityId) {
+    public Optional<WeatherMeasurement> findLastMeasurementForCity(UUID cityId) {
+        String query = "from(bucket: \"Development\") " +
+                "|> range(start: -1d) " +
+                "|> filter(fn: (r) => r._measurement == \"weather\" and r.cityID == \"" + cityId + "\" )" +
+                "|> filter(fn: (r) => r._field == \"humidity\" or r._field == \"temperature\" or r._field == \"pressure\")" +
+                "|> last() " +
+                "|> pivot(rowKey: [\"_time\"], columnKey: [\"_field\"], valueColumn: \"_value\")";
+
+        List<FluxTable> tables = influxDBClient.getQueryApi().query(query);
+        if(tables.size() == 0)
+            return Optional.empty();
+
+        FluxRecord result = tables.get(0).getRecords().get(0);
+
+        return mapFluxRecordToWeatherMeasurement(result, cityId);
+    }
+
+    public Optional<WeatherMeasurement> findLastDayAverageForCity(UUID cityId) {
         return findPeriodAverageForCity("1d", cityId);
     }
 
-    public WeatherMeasurement findLastWeekAverageForCity(UUID cityId) {
+    public Optional<WeatherMeasurement> findLastWeekAverageForCity(UUID cityId) {
         return findPeriodAverageForCity("7d", cityId);
     }
 
-    public WeatherMeasurement findLastTwoWeeksAverageForCity(UUID cityId) {
+    public Optional<WeatherMeasurement> findLastTwoWeeksAverageForCity(UUID cityId) {
         return findPeriodAverageForCity("14d", cityId);
     }
 
-    private WeatherMeasurement findPeriodAverageForCity(String period, UUID cityId) {
+    private Optional<WeatherMeasurement> findPeriodAverageForCity(String period, UUID cityId) {
         String query = "from(bucket: \"Development\") " +
                 "|> range(start: -" + period + ") " +
                 "|> filter(fn: (r) => r._measurement == \"weather\" and r.cityID == \"" + cityId + "\" )" +
@@ -54,22 +95,30 @@ public class WeatherRepository {
                 "|> pivot(rowKey: [\"_stop\"], columnKey: [\"_field\"], valueColumn: \"_value\")";
 
         List<FluxTable> tables = influxDBClient.getQueryApi().query(query);
+        if(tables.size() == 0)
+            return Optional.empty();
+
         FluxRecord result = tables.get(0).getRecords().get(0);
 
-       return mapFluxRecordToWeatherMeasurement(result, cityId);
+        return mapFluxRecordToWeatherMeasurement(result, cityId);
     }
 
-    private WeatherMeasurement mapFluxRecordToWeatherMeasurement(FluxRecord result, UUID cityId) {
+    private Optional<WeatherMeasurement> mapFluxRecordToWeatherMeasurement(FluxRecord result, UUID cityId) {
+        if(result == null)
+            return Optional.empty();
+
         double temperature = (double) result.getValueByKey("temperature");
         double humidity = (double) result.getValueByKey("humidity");
         double pressure = (double) result.getValueByKey("pressure");
+        Instant timestamp = result.getTime();
 
         WeatherMeasurement measurement = new WeatherMeasurement();
         measurement.setCityID(cityId);
+        measurement.setTimestamp(timestamp);
         measurement.setHumidity(temperature);
         measurement.setTemperature(humidity);
         measurement.setPressure(pressure);
 
-        return measurement;
+        return Optional.of(measurement);
     }
 }
